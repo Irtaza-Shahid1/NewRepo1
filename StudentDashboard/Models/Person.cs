1d814d6e12231397f607c329521d7b8cfaf43d2c
Irtaza-Shahid1 <irtazashahid1023@gmail.com>
2026-05-20 01:56:11 +0500

    added code in person

--- StudentDashboard/Models/Person.cs
+++ StudentDashboard/Models/Person.cs
@@ -1,6 +1,22 @@
-﻿namespace StudentDashboard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentDashboard.Web.Models;
+
+public abstract class Person : BaseEntity
 {
-    public class Person
-    {
-    }
+    [Required, StringLength(80)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required, StringLength(80)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required, EmailAddress, StringLength(150)]
+    public string Email { get; set; } = string.Empty;
+
+    [Phone, StringLength(30)]
+    public string Phone { get; set; } = string.Empty;
+
+    public virtual string GetFullName() => $"{FirstName} {LastName}".Trim();
+
+    public override string EntityLabel => GetFullName();
 }
