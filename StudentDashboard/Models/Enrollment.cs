2a999fb8785ec11fee532a9d515c58cd92c4a204
Irtaza-Shahid1 <irtazashahid1023@gmail.com>
2026-05-20 01:47:50 +0500

    added code in enrollement

--- StudentDashboard/Models/Enrollment.cs
+++ StudentDashboard/Models/Enrollment.cs
@@ -1,6 +1,21 @@
-﻿namespace StudentDashboard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentDashboard.Web.Models;
+
+public class Enrollment : BaseEntity
 {
-    public class Enrollment
-    {
-    }
+    [Required]
+    public int StudentId { get; set; }
+
+    public Student? Student { get; set; }
+
+    [Required]
+    public int CourseId { get; set; }
+
+    public Course? Course { get; set; }
+
+    public DateTime EnrolledOn { get; set; } = DateTime.Today;
+
+    [StringLength(30)]
+    public string Status { get; set; } = "Enrolled";
 }
