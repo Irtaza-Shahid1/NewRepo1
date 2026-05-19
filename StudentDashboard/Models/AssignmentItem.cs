707f03f7444a848433eef1e1adad12704f38b985
Irtaza-Shahid1 <irtazashahid1023@gmail.com>
2026-05-20 01:37:35 +0500

    added code in assignment file

--- StudentDashboard/Models/AssignmentItem.cs
+++ StudentDashboard/Models/AssignmentItem.cs
@@ -1,6 +1,21 @@
-﻿namespace StudentDashboard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentDashboard.Web.Models;
+
+public class AssignmentItem : BaseEntity
 {
-    public class AssignmentItem
-    {
-    }
+    [Required]
+    public int CourseId { get; set; }
+    public Course? Course { get; set; }
+
+    [Required, StringLength(120)]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime DueDate { get; set; } = DateTime.Today.AddDays(7);
+
+    [Range(1, 1000)]
+    public decimal TotalMarks { get; set; } = 100;
 }
