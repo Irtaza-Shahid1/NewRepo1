12ad45c613e7ec45b55ee87b01bdd64369d1bedf
Irtaza-Shahid1 <irtazashahid1023@gmail.com>
2026-05-20 02:12:57 +0500

    added code in IRepository

--- StudentDashboard/Repositries/IRepository.cs
+++ StudentDashboard/Repositries/IRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace StudentDashboard.Repositries
+﻿using StudentDashboard.Web.Models;
+
+namespace StudentDashboard.Web.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
 {
-    public class IRepository
-    {
-    }
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
 }
