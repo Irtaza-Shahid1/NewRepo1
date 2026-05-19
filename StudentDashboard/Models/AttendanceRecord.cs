fe5c92ce45b39db548503f87395ab2a32eea3d49
Irtaza-Shahid1 <irtazashahid1023@gmail.com>
2026-05-20 01:40:05 +0500

    added code in attendancerecord

--- StudentDashboard/Models/AttendanceRecord.cs
+++ StudentDashboard/Models/AttendanceRecord.cs
@@ -1,6 +1,22 @@
-﻿namespace StudentDashboard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentDashboard.Web.Models;
+
+public class AttendanceRecord : BaseEntity
 {
-    public class AttendanceRecord
-    {
-    }
+    [Required]
+    public int StudentId { get; set; }
+    public Student? Student { get; set; }
+
+    [Required]
+    public int CourseId { get; set; }
+    public Course? Course { get; set; }
+
+    public DateTime AttendanceDate { get; set; } = DateTime.Today;
+
+    [Required, StringLength(20)]
+    public string Status { get; set; } = "Present";
+
+    [StringLength(250)]
+    public string Remarks { get; set; } = string.Empty;
 }
