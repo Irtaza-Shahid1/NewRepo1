711cdcc82ff9b8d65fdf8e85cbcafe3250cae008
Irtaza-Shahid1 <irtazashahid1023@gmail.com>
2026-05-20 02:18:42 +0500

    added code in dashboard

--- StudentDashboard/Services/DashboardState.cs
+++ StudentDashboard/Services/DashboardState.cs
@@ -1,6 +1,13 @@
-﻿namespace StudentDashboard.Services
+﻿namespace StudentDashboard.Web.Services;
+
+public class DashboardState
 {
-    public class DashboardState
+    public string LastMessage { get; private set; } = string.Empty;
+    public event Action? OnChange;
+
+    public void SetMessage(string message)
     {
+        LastMessage = message;
+        OnChange?.Invoke();
     }
 }
